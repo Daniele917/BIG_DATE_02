697c8e96ba5547bf59b82b29de294b6c9b7de02b
Daniele917 <danieledecarvalhorodrigues46@gmail.com>
2024-10-08 18:43:00 -0300

    Add files via upload

--- AULA_06/exercicio_04.py
+++ AULA_06/exercicio_04.py
@@ -1,16 +1,16 @@
 # Sistema de Autenticação de Usuários : 
 
 
-usuarios = ["Pedro","Maria","Duda","Dirceu","Elvis"]
-senhas = ["123","345","567","@@@$$$","8910"]
+usuarios = ["Creuza","Daniele","Julio","Valquiria","João"]
+senhas = ["1234","0000","2209","0203","0304"]
 usuario = input("Informe o nome de acesso ao sistema: ")
 for i in range(len(usuarios)):
     if usuarios[i] == usuario:
         senha = input("Informe a senha de acesso:")
         if senhas[i] == senha:
-          resp = "Acesso Liberado"
+          resp = "Senha Confere - Acesso Liberado"
         else:
-            resp = ("Senha não confere")
+            resp = ("Senha não confere - Aceeso Negado")
         break
     else:
         resp = "Usuário não Encontrado"
